1de5bc32fc68d77a7a50d6a10e3c945ec8612cda
Thanapat <t.veerakajorn@gmail.com>
2021-03-02 14:26:46 -0500

    commit tree 1b5da27698d9c147170950a047c0afc18f5d67f9

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -24,6 +24,7 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -51,4 +52,9 @@ export class RestaurantInput extends Component {
 
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: restaurant => dispatch(addRestaurant(restaurant))
+  }
+}
+export default connect(null, mapDispatchToProps)(RestaurantInput)
